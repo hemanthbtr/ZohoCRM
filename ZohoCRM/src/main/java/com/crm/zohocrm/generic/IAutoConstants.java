550747ca95c61b45e84c9a39668f464ac953b83d
hemanthbtr <79132676+hemanthbtr@users.noreply.github.com>
2021-03-12 17:29:17 +0530

    Update IAutoConstants.java

--- ZohoCRM/src/main/java/com/crm/zohocrm/generic/IAutoConstants.java
+++ ZohoCRM/src/main/java/com/crm/zohocrm/generic/IAutoConstants.java
@@ -3,7 +3,7 @@ package com.crm.zohocrm.generic;
 public interface IAutoConstants {
 	String DEFAULT_BROWSER="firefox";
 	
-	String APP_URL="http://14.99.190.22:7777/";
+	String APP_URL="http://192.168.1.52:8080/crm/HomePage.do";
 	String CHROME_KEY="webdriver.chrome.driver";
 	String CHROME_PATH="./drivers/chromedriver.exe";
 	String GECKO_KEY="webdriver.gecko.driver";
